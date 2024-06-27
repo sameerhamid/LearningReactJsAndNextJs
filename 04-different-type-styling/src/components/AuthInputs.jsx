80a9a792b2db166d6ea496dcba50ade04de8e7f9
inder <sameerhamid213@gmail.com>
2024-06-27 16:23:02 +0530

    learned how to add pseudo selctors , nested rules and media quries to styled components

--- 04-different-type-styling/src/components/AuthInputs.jsx
+++ 04-different-type-styling/src/components/AuthInputs.jsx
@@ -1,41 +1,57 @@
-import { useState } from 'react';
-import { styled } from 'styled-components'
+import { useState } from "react";
+import { styled } from "styled-components";
 
 const ControlledDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    margin-bottom: 1.5rem;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+`;
 
 const StyledLabel = styled.label`
-        display: block;
-        margin-bottom: 0.5rem;
-        font-size: 0.75rem;
-        font-weight: 700;
-        letter-spacing: 0.1em;
-        text-transform: uppercase;
-        color: ${({ $invalid }) => $invalid ? "#f87171" : "#6b7280"};
-`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
+`;
+
+// & if used with space it will target its chilrens else without space it will be targe itself for psuedo selectors
+const StyledButton = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
 
 const StyledInput = styled.input`
-      width: 100%;
-      padding: 0.75rem 1rem;
-      line-height: 1.5;
-      background-color: ${({ $invalid }) => $invalid ? "#fed2d2" : "#d1d5db"};
-      color: ${({ $invalid }) => $invalid ? "#ef4444" : "#374151"};
-      border: 1px solid ${({ $invalid }) => $invalid ? "#f73f3f" : 'transparent'};
-      border-radius: 0.25rem;
-      box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+  width: 100%;
+  padding: 0.75rem 1rem;
+  line-height: 1.5;
+  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
+  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
+  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
+  border-radius: 0.25rem;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
 
 export default function AuthInputs() {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   function handleInputChange(identifier, value) {
-    if (identifier === 'email') {
+    if (identifier === "email") {
       setEnteredEmail(value);
     } else {
       setEnteredPassword(value);
@@ -46,7 +62,7 @@ export default function AuthInputs() {
     setSubmitted(true);
   }
 
-  const emailNotValid = submitted && !enteredEmail.includes('@');
+  const emailNotValid = submitted && !enteredEmail.includes("@");
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
@@ -58,7 +74,7 @@ export default function AuthInputs() {
           <StyledInput
             type="email"
             $invalid={emailNotValid}
-            onChange={(event) => handleInputChange('email', event.target.value)}
+            onChange={(event) => handleInputChange("email", event.target.value)}
           />
         </p>
         <p>
@@ -67,7 +83,7 @@ export default function AuthInputs() {
             type="password"
             $invalid={passwordNotValid}
             onChange={(event) =>
-              handleInputChange('password', event.target.value)
+              handleInputChange("password", event.target.value)
             }
           />
         </p>
@@ -77,7 +93,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <StyledButton onClick={handleLogin}>Sign In</StyledButton>
       </div>
     </div>
   );
