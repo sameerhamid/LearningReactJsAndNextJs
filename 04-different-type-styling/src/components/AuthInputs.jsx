b3d6a1eec433947bc9c23cb7c02e0aead9a9948b
inder <sameerhamid213@gmail.com>
2024-06-27 16:05:19 +0530

    learned dynamic and conditional styling with styled components
    learned to add  $ sign to custom props to pass styled components for not crashing with existing props

--- 04-different-type-styling/src/components/AuthInputs.jsx
+++ 04-different-type-styling/src/components/AuthInputs.jsx
@@ -15,16 +15,16 @@ const StyledLabel = styled.label`
         font-weight: 700;
         letter-spacing: 0.1em;
         text-transform: uppercase;
-        color: #6b7280;
+        color: ${({ $invalid }) => $invalid ? "#f87171" : "#6b7280"};
 `
 
 const StyledInput = styled.input`
       width: 100%;
       padding: 0.75rem 1rem;
       line-height: 1.5;
-      background-color: #d1d5db;
-      color: #374151;
-      border: 1px solid transparent;
+      background-color: ${({ $invalid }) => $invalid ? "#fed2d2" : "#d1d5db"};
+      color: ${({ $invalid }) => $invalid ? "#ef4444" : "#374151"};
+      border: 1px solid ${({ $invalid }) => $invalid ? "#f73f3f" : 'transparent'};
       border-radius: 0.25rem;
       box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -54,18 +54,18 @@ export default function AuthInputs() {
       {/* <div className="controls"> */}
       <ControlledDiv>
         <p>
-          <StyledLabel className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</StyledLabel>
+          <StyledLabel $invalid={emailNotValid}>Email</StyledLabel>
           <StyledInput
             type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            $invalid={emailNotValid}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <StyledLabel className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</StyledLabel>
+          <StyledLabel $invalid={passwordNotValid}>Password</StyledLabel>
           <StyledInput
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            $invalid={passwordNotValid}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
