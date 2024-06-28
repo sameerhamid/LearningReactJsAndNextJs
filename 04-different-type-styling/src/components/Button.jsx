611bc3ec4e5ef5b7588a639d3b7e5aae5d901a1e
inder <sameerhamid213@gmail.com>
2024-06-28 16:52:36 +0530

    add tailwind classes in button comp

--- 04-different-type-styling/src/components/Button.jsx
+++ 04-different-type-styling/src/components/Button.jsx
@@ -1,19 +1,36 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-// & if used with space it will target its chilrens else without space it will be targe itself for psuedo selectors
-const StyledButton = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
+// // & if used with space it will target its chilrens else without space it will be targe itself for psuedo selectors
+// const StyledButton = styled.button`
+//   padding: 1rem 2rem;
+//   font-weight: 600;
+//   text-transform: uppercase;
+//   border-radius: 0.25rem;
+//   color: #1f2937;
+//   background-color: #f0b322;
+//   border-radius: 6px;
+//   border: none;
 
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
+//   &:hover {
+//     background-color: #f0920e;
+//   }
+// `;
 
-export default StyledButton
+// export default StyledButton
+
+import React from "react";
+
+function Button({ children, ...props }) {
+  return (
+    <button {...props} className="px-4 py-2 
+    font-semibold uppercase rounded
+    text-stone-900
+    bg-amber-400
+    hover:bg-amber-500
+    ">
+      {children}
+    </button>
+  );
+}
+
+export default Button;
