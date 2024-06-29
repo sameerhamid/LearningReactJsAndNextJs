ec871283c553411ed440349b3e58a5444e36fce7
inder <sameerhamid213@gmail.com>
2024-06-29 12:09:58 +0530

    learn about refs

--- 06-count-down/src/components/Player.jsx
+++ 06-count-down/src/components/Player.jsx
@@ -1,10 +1,17 @@
+import { useState, useRef } from 'react'
 export default function Player() {
+  const inputRef = useRef()
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null)
+
+  const handleClick = () => {
+    setEnteredPlayerName(inputRef.current.value)
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={inputRef} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
