5d0bedb7153861039718cbed0a59be3b787974e3
inder <sameerhamid213@gmail.com>
2024-06-29 12:57:02 +0530

    add state timeExpired and timerStarted
    add handleStart method to start the time

--- 06-count-down/src/components/TimerChallange.jsx
+++ 06-count-down/src/components/TimerChallange.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 
 function TimerChallange({ title, targetTime }) {
+
+    const [timerExpired, setTimerExpired] = React.useState(false)
+    const [timerStarted, setTimerStarted] = React.useState(false)
+    const handleStart = () => {
+        setTimerStarted(true)
+        setTimeout(() => {
+            setTimerExpired(true)
+        }, targetTime * 1000)
+    }
+
     return (
         <section className='challenge'>
             <h2>{title}</h2>
+            {timerExpired && <p>You lost!</p>}
             <p className='challenge-time'>{targetTime} second{targetTime > 1 ? 's' : ""}</p>
 
             <p>
-                <button>Start challenge</button>
+                <button onClick={handleStart}>{timerStarted ? "Stop" : "Start"} challenge</button>
             </p>
-            <p className=''>
-                Time is running ...
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? 'Time is running ...' : "Timer inactive"}
             </p>
         </section>
     )
