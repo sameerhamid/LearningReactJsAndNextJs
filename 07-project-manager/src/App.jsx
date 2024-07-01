2cc333736ed082689324b18167b0879bbc5a4355
inder <sameerhamid213@gmail.com>
2024-07-01 16:49:51 +0530

    add custom Button component
    add NoPorjects component with tailwind css classes

--- 07-project-manager/src/App.jsx
+++ 07-project-manager/src/App.jsx
@@ -1,11 +1,13 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelcted from "./components/NoProjectSelcted";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      {/* <NewProject /> */}
+      <NoProjectSelcted />
     </main>
   );
 }
