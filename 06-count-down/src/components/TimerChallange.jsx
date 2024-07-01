f8180eb465c454382305317b7f7b7c86eb818c81
inder <sameerhamid213@gmail.com>
2024-07-01 10:52:55 +0530

    add resultModal

--- 06-count-down/src/components/TimerChallange.jsx
+++ 06-count-down/src/components/TimerChallange.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import ResultModal from './ResultModal'
 
 function TimerChallange({ title, targetTime }) {
+    const timer = useRef()
 
     const [timerExpired, setTimerExpired] = React.useState(false)
     const [timerStarted, setTimerStarted] = React.useState(false)
+
     const handleStart = () => {
         setTimerStarted(true)
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true)
         }, targetTime * 1000)
     }
 
+    const handleStop = () => {
+        clearTimeout(timer.current)
+    }
     return (
-        <section className='challenge'>
-            <h2>{title}</h2>
-            {timerExpired && <p>You lost!</p>}
-            <p className='challenge-time'>{targetTime} second{targetTime > 1 ? 's' : ""}</p>
+        <>
+            {timerExpired && <ResultModal targetTime={targetTime} result="lost" />}
+            <section className='challenge'>
+                <h2>{title}</h2>
+                <p className='challenge-time'>{targetTime} second{targetTime > 1 ? 's' : ""}</p>
 
-            <p>
-                <button onClick={handleStart}>{timerStarted ? "Stop" : "Start"} challenge</button>
-            </p>
-            <p className={timerStarted ? 'active' : undefined}>
-                {timerStarted ? 'Time is running ...' : "Timer inactive"}
-            </p>
-        </section>
+                <p>
+                    <button onClick={timerStarted ? handleStop : handleStart}>{timerStarted ? "Stop" : "Start"} challenge</button>
+                </p>
+                <p className={timerStarted ? 'active' : undefined}>
+                    {timerStarted ? 'Time is running ...' : "Timer inactive"}
+                </p>
+            </section>
+        </>
     )
 }
 
