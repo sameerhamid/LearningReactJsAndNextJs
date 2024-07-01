a9a480b07a4749f90cde5bf54f8ca32b008b1c5a
inder <sameerhamid213@gmail.com>
2024-07-01 12:59:01 +0530

    add handleReset method
    add handleStart
    
    add handleStop

--- 06-count-down/src/components/ResultModal.jsx
+++ 06-count-down/src/components/ResultModal.jsx
@@ -1,7 +1,9 @@
 import React, { forwardRef, useImperativeHandle, useRef } from 'react'
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
     const dialogRef = useRef()
+    const userLost = remainingTime <= 0
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
 
     useImperativeHandle(ref, () => {
         return {
@@ -10,10 +12,10 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
     })
     return (
         <dialog className='result-modal' ref={dialogRef}>
-            <h2>You {result}</h2>
+            {userLost && <h2>You lost</h2>}
             <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-            <p>You stopped the timer with <strong>X seconds left.</strong></p>
-            <form method='dialog'>
+            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+            <form method='dialog' onSubmit={onReset}>
                 <button>Close</button>
             </form>
         </dialog>
--- 06-count-down/src/components/TimerChallange.jsx
+++ 06-count-down/src/components/TimerChallange.jsx
@@ -6,35 +6,43 @@ function TimerChallange({ title, targetTime }) {
     const dialogRef = useRef()
 
     const [timeRemaining, setTimeRemaining] = useState(targetTime)
+    console.log(timeRemaining);
     const timerAcitve = timeRemaining > 0 && timeRemaining < targetTime * 1000
 
+    console.log(timerAcitve);
+
     if (timeRemaining <= 0) {
+
         clearInterval(timerRef.current)
-        setTimeRemaining(targetTime * 1000)
         dialogRef.current.open()
     }
 
     const handleStart = () => {
-
         timerRef.current = setInterval(() => {
             setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10)
-        }, 10)
+        }, 1)
+    }
+
+    const handleReset = () => {
+        setTimeRemaining(targetTime * 1000)
     }
 
     const handleStop = () => {
+        console.log('called');
         dialogRef.current.open()
         clearInterval(timerRef.current)
-
     }
     return (
         <>
-            <ResultModal targetTime={targetTime} result="lost" ref={dialogRef} />
+            <ResultModal targetTime={targetTime}
+                onReset={handleReset}
+                remainingTime={timeRemaining} ref={dialogRef} />
             <section className='challenge'>
                 <h2>{title}</h2>
                 <p className='challenge-time'>{targetTime} second{targetTime > 1 ? 's' : ""}</p>
 
                 <p>
-                    <button onClick={timerAcitve ? handleStop : handleStart}>{timerStarted ? "Stop" : "Start"} challenge</button>
+                    <button onClick={timerAcitve ? handleStop : handleStart}>{timerAcitve ? "Stop" : "Start"} challenge</button>
                 </p>
                 <p className={timerAcitve ? 'active' : undefined}>
                     {timerAcitve ? 'Time is running ...' : "Timer inactive"}
