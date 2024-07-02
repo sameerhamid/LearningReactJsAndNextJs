a63d856722354665d2ade5ed75e021caf3c02582
inder <sameerhamid213@gmail.com>
2024-07-02 15:12:15 +0530

    add NewTask comp

--- 07-project-manager/src/components/Tasks.jsx
+++ 07-project-manager/src/components/Tasks.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NewTask from './NewTask'
 
 function Tasks() {
     return (
         <section>
-            <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
-            New Task
+            <h2 className='text-2xl font-bold text-stone-700 my-4'>Tasks</h2>
+            <NewTask />
             <p className='text-stone-800 mb-4'>This project does not have any tasks yet.</p>
             <ul></ul>
         </section>
