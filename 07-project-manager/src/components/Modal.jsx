93b403a655770c7a894d464bd0490d69ea6950bd
inder <sameerhamid213@gmail.com>
2024-07-02 12:09:50 +0530

    add modal comp

---
+++ 07-project-manager/src/components/Modal.jsx
@@ -0,0 +1,24 @@
+import React, { forwardRef, useImperativeHandle, useRef } from 'react'
+import { createPortal } from 'react-dom'
+const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
+    const dialogRef = useRef()
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialogRef.current.showModal()
+            }
+        }
+    })
+    return (
+        createPortal(
+            <dialog ref={dialogRef}>
+                {children}
+                <form method='dialog'>
+                    <button>{buttonCaption}</button>
+                </form>
+            </dialog>, document.getElementById('modal-root')
+        )
+    )
+}
+)
+export default Modal
--- 07-project-manager/src/components/NewProject.jsx
+++ 07-project-manager/src/components/NewProject.jsx
@@ -1,18 +1,22 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 function NewProject({ onAddProject }) {
     const titleRef = useRef('');
     const descriptionRef = useRef('');
     const dueDateRef = useRef('');
+    const modalRef = useRef(null)
 
     const handleSave = () => {
         const enteredTitle = titleRef.current.value;
         const enteredDescription = descriptionRef.current.value;
         const enteredDueDate = dueDateRef.current.value;
 
-        if (enteredTitle === '' || enteredDescription === '' || enteredDueDate === ''
+        if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === ''
         ) {
+            // show the error modal
+            modalRef.current.open()
             return
         }
 
@@ -28,34 +32,41 @@ function NewProject({ onAddProject }) {
         dueDateRef.current.value = '';
     };
     return (
-        <div className="w-[35rem] mt-60">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-stone-800 hover:text-stone-950">
-                        Cancel
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={handleSave}
-                        className="px-6 py-2 rounded bg-stone-800 text-stone-50 hover:bg-stone-950"
-                    >
-                        Save
-                    </button>
-                </li>
-            </menu>
+        <>
+            <Modal ref={modalRef} buttonCaption="Close">
+                <h2>Invalid Input</h2>
+                <p>Oops ... looks like you forgot to enter a value.</p>
+                <p>Please make sure you provide a valid value for every input field.</p>
+            </Modal>
+            <div className="w-[35rem] mt-60">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li>
+                        <button className="text-stone-800 hover:text-stone-950">
+                            Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={handleSave}
+                            className="px-6 py-2 rounded bg-stone-800 text-stone-50 hover:bg-stone-950"
+                        >
+                            Save
+                        </button>
+                    </li>
+                </menu>
 
-            <div>
-                {/* first input */}
-                <Input ref={titleRef} label="Title" type="text" />
-                {/* second input */}
+                <div>
+                    {/* first input */}
+                    <Input ref={titleRef} label="Title" type="text" />
+                    {/* second input */}
 
-                <Input ref={descriptionRef} label="Description" isTextArea />
-                {/* third input */}
+                    <Input ref={descriptionRef} label="Description" isTextArea />
+                    {/* third input */}
 
-                <Input ref={dueDateRef} label="Due Date" type="date" />
+                    <Input ref={dueDateRef} label="Due Date" type="date" />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
