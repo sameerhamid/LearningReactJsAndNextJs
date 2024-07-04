1eb053997a5f08b840d95764a51bee109ed03ef4
inder <sameerhamid213@gmail.com>
2024-07-04 12:22:49 +0530

    add quiz compoent

--- 10-quiz-app/src/components/Quiz.jsx
+++ 10-quiz-app/src/components/Quiz.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import QUESTIONS from '../questions'
 function Quiz() {
-    return (
-        <div>
+    const [userAnswers, setUserAnswers] = useState([])
+    const activeQuestionIndex = userAnswers.length
 
+    const handleSelectAnswer = (selectedAnswer) => {
+        setUserAnswers(prevAnsers => {
+            return [...prevAnsers, selectedAnswer]
+        })
+    }
+    return (
+        <div id='quiz'>
+            <div id='question'>
+                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map(answer => {
+                        return <li key={answer} className='answer'>
+                            <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                        </li>
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
