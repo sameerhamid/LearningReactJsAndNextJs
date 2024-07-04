656b4b64b557033970a174b8e15c6488b877d831
inder <sameerhamid213@gmail.com>
2024-07-04 12:41:21 +0530

    add check for is Quiz complet to render quiez complete

--- 10-quiz-app/src/components/Quiz.jsx
+++ 10-quiz-app/src/components/Quiz.jsx
@@ -1,20 +1,35 @@
 import React, { useState } from 'react'
 import QUESTIONS from '../questions'
+import quizCompleteImg from '../assets/quiz-complete.png'
 function Quiz() {
     const [userAnswers, setUserAnswers] = useState([])
     const activeQuestionIndex = userAnswers.length
+
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length
 
     const handleSelectAnswer = (selectedAnswer) => {
         setUserAnswers(prevAnsers => {
             return [...prevAnsers, selectedAnswer]
         })
     }
+
+
+    if (quizIsComplete) {
+        return (
+            <div id='summary'>
+                <img src={quizCompleteImg} alt="Quiz complete image" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        )
+    }
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() - 0.5)
     return (
         <div id='quiz'>
             <div id='question'>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer => {
+                    {shuffledAnswers.map(answer => {
                         return <li key={answer} className='answer'>
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
