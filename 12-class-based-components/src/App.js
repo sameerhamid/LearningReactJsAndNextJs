c1b41e9b6eac8073a006ae67389dccd44862a834
inder <sameerhamid213@gmail.com>
2024-07-08 13:18:58 +0530

    convert user and app component into Class based

--- 12-class-based-components/src/App.js
+++ 12-class-based-components/src/App.js
@@ -1,11 +1,22 @@
-import Users from './components/Users';
+import { Component } from "react";
+import Users from "./components/Users";
 
-function App() {
-  return (
-    <div>
-      <Users />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Users />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Users />
+      </div>
+    );
+  }
 }
 
 export default App;
