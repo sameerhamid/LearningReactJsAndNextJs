3562d49e14257db88df9c78b0d55106bc3eb73f0
inder <sameerhamid213@gmail.com>
2024-07-08 17:40:09 +0530

    fetch places for availablePlaces component

--- 13-place-picker-usin-dummy-api/src/components/AvailablePlaces.jsx
+++ 13-place-picker-usin-dummy-api/src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([])
+
+
+  const fetchPlaces = async () => {
+    const response = await fetch('http://localhost:3000/places');
+    const data = await response.json();
+    setAvailablePlaces(data.places)
+  }
+
+  useEffect(() => {
+    fetchPlaces()
+  }, [])
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
