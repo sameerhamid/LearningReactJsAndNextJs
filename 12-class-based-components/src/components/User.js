b8d2638b66ed2ee1ed0bfd537bdf47da9072bceb
inder <sameerhamid213@gmail.com>
2024-07-08 13:27:24 +0530

    convert users component into class based

--- 12-class-based-components/src/components/User.js
+++ 12-class-based-components/src/components/User.js
@@ -6,8 +6,8 @@ import classes from "./User.module.css";
 // };
 
 class User extends Component {
-  render(props) {
-    return <li className={classes.user}>{props.name}</li>;
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
   }
 }
 
