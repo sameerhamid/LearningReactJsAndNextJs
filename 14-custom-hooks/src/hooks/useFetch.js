412d74ea52d0f451e85023abe4620e43e447a5d6
inder <sameerhamid213@gmail.com>
2024-07-09 13:06:23 +0530

    add custom hook useFetch

---
+++ 14-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+
+function useFetch(fetchFn, initilaValue) {
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
+  const [fetchedData, setFetchedData] = useState(initilaValue);
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setFetchedData(data);
+      } catch (error) {
+        setError({
+          message: error.message || "Failed to fetch data.",
+        });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+
+  return {
+    isFetching,
+    error,
+    fetchedData,
+  };
+}
+
+export default useFetch;
