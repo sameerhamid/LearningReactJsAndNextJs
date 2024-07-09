01e404c49b7e88c6d55f03a7619224a790ae8bf1
inder <sameerhamid213@gmail.com>
2024-07-09 16:24:02 +0530

    add states for email input and password input

--- 15-react-forms/src/components/Login.jsx
+++ 15-react-forms/src/components/Login.jsx
@@ -1,17 +1,40 @@
+import React, { useState } from 'react'
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState('')
+  const [enteredPassword, setEnteredPassword] = useState('')
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("email:>>>", enteredEmail);
+    console.log("password:>>>", enteredPassword);
+  }
+
+  const handleEmailChange = (event) => {
+    setEnteredEmail(event.target.value)
+  }
+  const handlePasswordChange = (event) => {
+    setEnteredPassword(event.target.value)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email"
+            value={enteredEmail}
+            onChange={handleEmailChange}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password"
+            value={enteredPassword}
+            onChange={handlePasswordChange}
+          />
         </div>
       </div>
 
