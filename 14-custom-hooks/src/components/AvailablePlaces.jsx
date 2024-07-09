630ac5042fad6c5bbbff9a74b6853260d3f71b44
inder <sameerhamid213@gmail.com>
2024-07-09 13:38:58 +0530

    add fetchSortedPlaces

--- 14-custom-hooks/src/components/AvailablePlaces.jsx
+++ 14-custom-hooks/src/components/AvailablePlaces.jsx
@@ -1,10 +1,25 @@
-import { useState, useEffect } from 'react';
-
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 import useFetch from '../hooks/useFetch.js';
+
+const fetchSortedPlaces = async () => {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+
+    });
+  })
+
+}
 
 export default function AvailablePlaces({ onSelectPlace }) {
   // const [isFetching, setIsFetching] = useState(false);
@@ -13,7 +28,7 @@ export default function AvailablePlaces({ onSelectPlace }) {
 
   const { isFetching,
     error,
-    fetchedData: availablePlaces } = useFetch(fetchAvailablePlaces, [])
+    fetchedData: availablePlaces } = useFetch(fetchSortedPlaces, [])
 
   // useEffect(() => {
   //   async function fetchPlaces() {
