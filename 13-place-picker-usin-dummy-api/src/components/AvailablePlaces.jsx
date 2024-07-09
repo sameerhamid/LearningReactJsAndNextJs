73fe90bed05fdc2473a20476215149214752326a
inder <sameerhamid213@gmail.com>
2024-07-09 10:13:30 +0530

    add loading state and loadingText in AvailablePlaces comp

--- 13-place-picker-usin-dummy-api/src/components/AvailablePlaces.jsx
+++ 13-place-picker-usin-dummy-api/src/components/AvailablePlaces.jsx
@@ -3,12 +3,15 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([])
+  const [loading, setLoading] = useState(true)
 
 
   const fetchPlaces = async () => {
+    setLoading(true)
     const response = await fetch('http://localhost:3000/places');
     const data = await response.json();
     setAvailablePlaces(data.places)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -17,6 +20,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
   return (
     <Places
       title="Available Places"
+      isLoading={loading}
+      loadingText="Fetching places data..."
       places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
