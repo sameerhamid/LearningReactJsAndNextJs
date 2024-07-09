15c4d38cefd7f53088512efca953eb437d921a83
inder <sameerhamid213@gmail.com>
2024-07-09 16:49:56 +0530

    add signup comp

--- 15-react-forms/src/components/Login.jsx
+++ 15-react-forms/src/components/Login.jsx
@@ -1,17 +1,26 @@
+import React, { useRef } from 'react'
 export default function Login() {
+
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(emailRef.current.value)
+    console.log(passwordRef.current.value)
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" ref={emailRef} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" ref={passwordRef} />
         </div>
       </div>
 
