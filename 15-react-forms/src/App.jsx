02601e9dbb307355f7f013b4bcc40797186d483a
inder <sameerhamid213@gmail.com>
2024-07-10 11:34:06 +0530

    add built in form validation on signup form

--- 15-react-forms/src/App.jsx
+++ 15-react-forms/src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     <>
       <Header />
       <main>
-        <Login />
-        {/* <Signup /> */}
+        {/* <Login /> */}
+        <Signup />
         {/* <LoginUsingState /> */}
       </main>
     </>
