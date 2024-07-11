81053abf0096b8e84e11144aad99a6f30c6d0e23
inder <sameerhamid213@gmail.com>
2024-07-11 16:10:08 +0530

    add error comp
    add loading and error comp in meals comp

--- 16-food-order-app/src/components/Meals.jsx
+++ 16-food-order-app/src/components/Meals.jsx
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import MealItem from './MealItem'
 import useHttp from '../hooks/useHttp'
+import Error from './UI/Error'
 const requestConfig = {}
 function Meals() {
     const { data: loadedMeals, isLoading, error } = useHttp('meals', requestConfig, [])
 
+    if (isLoading) {
+        return <p className='center'>Fething meals...</p>
+    }
+
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error} />
+    }
     return (
         <ul id='meals'>
             {loadedMeals.map(meal => {
