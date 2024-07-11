666b1c83b33bc54d33b4afdb7f1c01986138951c
inder <sameerhamid213@gmail.com>
2024-07-11 10:18:32 +0530

    add CartItem comp

--- 16-food-order-app/src/components/Cart.jsx
+++ 16-food-order-app/src/components/Cart.jsx
@@ -1,48 +1,50 @@
-import React, { useContext } from 'react'
-import Modal from './UI/Modal'
-import CartContext from '../store/CartContext'
-import { currencyFormatter } from '../utils/formatting'
-import Button from './UI/Button'
-import UserProgressContext from '../store/UserProgressContext'
+import React, { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../utils/formatting";
+import Button from "./UI/Button";
+import UserProgressContext from "../store/UserProgressContext";
+import CartItem from "./CartItem";
 
 function Cart() {
-    const { items } = useContext(CartContext)
-    const { progress, hideCart } = useContext(UserProgressContext)
+    const { items, addItem, removeItem } = useContext(CartContext);
+    const { progress, hideCart, } = useContext(UserProgressContext);
     const cartTotal = items.reduce((totalPrice, item) => {
-        return totalPrice + item.quantity * item.price
-    }, 0)
-
+        return totalPrice + item.quantity * item.price;
+    }, 0);
 
     const handleCloseCart = () => {
-
-        hideCart()
-    }
-
-
+        hideCart();
+    };
 
     return (
-        <Modal className='cart' open={progress === 'cart'}>
+        <Modal className="cart" open={progress === "cart"}>
             <h2>Your Cart</h2>
             <ul>
-                {items.map(item => {
+                {items.map((item) => {
                     return (
-                        <li key={item.id}>
-                            <span>{item.name}</span>
-                            <span>-</span>
-                            <span>x {item.quantity}</span>
-                        </li>
-                    )
+                        <CartItem
+                            key={item.id}
+                            name={item.name}
+                            quantity={item.quantity}
+                            price={item.price}
+                            onDecrease={() =>
+                                removeItem(item.id)
+                            }
+                            onIncrease={() => addItem(item)}
+                        />
+                    );
                 })}
             </ul>
-            <p className='cart-total'>
-                {currencyFormatter.format(cartTotal)}
-            </p>
-            <p className='modal-actions'>
-                <Button textOnly onClick={handleCloseCart}>Close</Button>
+            <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+            <p className="modal-actions">
+                <Button textOnly onClick={handleCloseCart}>
+                    Close
+                </Button>
                 <Button>Go to Checkout</Button>
             </p>
         </Modal>
-    )
+    );
 }
 
-export default Cart
+export default Cart;
