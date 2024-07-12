412e1c7c873e3a7d135f653415c0bc7cf62119b7
inder <sameerhamid213@gmail.com>
2024-07-12 16:06:44 +0530

    add store

--- 17-counter-with-redux/src/App.js
+++ 17-counter-with-redux/src/App.js
@@ -1,9 +1,12 @@
-import Counter from './components/Counter';
-
+import Counter from "./components/Counter";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Counter />
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 }
 
