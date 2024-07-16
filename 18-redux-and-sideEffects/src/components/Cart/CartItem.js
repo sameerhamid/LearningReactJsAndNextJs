8c0b4bedb34bdab8cdf08f4be9dfec57466df384
inder <sameerhamid213@gmail.com>
2024-07-16 12:01:51 +0530

    add logic in addCartItemHandler in cartItem

--- 18-redux-and-sideEffects/src/components/Cart/CartItem.js
+++ 18-redux-and-sideEffects/src/components/Cart/CartItem.js
@@ -1,22 +1,49 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import classes from "./CartItem.module.css";
 import { cartActions } from "../../store/cart-slice";
 
 const CartItem = (props) => {
   const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
   const { name, quantity, totalPrice, price, id } = props;
 
   const removeCartItemHandler = () => {
     dispatch(cartActions.removeItem(id));
   };
   const addCartItemHanlder = () => {
-    dispatch(
-      cartActions.addItem({
-        id,
-        price,
-        title: name,
-      })
-    );
+    const newTotalQuantity = cart.totalQuantity + 1;
+    const updatedItems = cart.items.slice();
+    const existingItem = updatedItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem };
+      updatedItem.quantity++;
+      updatedItem.price = updatedItem.price + price;
+      const existingItemIndex = updatedItems.findIndex(
+        (item) => item.id === id
+      );
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: name,
+      });
+    }
+    const newCart = {
+      items: updatedItems,
+      totalQuantity: newTotalQuantity,
+    };
+
+    dispatch(cartActions.replaceCart(newCart));
+    // dispatch(
+    //   cartActions.addItem({
+    //     id,
+    //     price,
+    //     title: name,
+    //   })
+    // );
   };
   return (
     <li className={classes.item}>
