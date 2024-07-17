f3ec5720e2dbead564e32440f8d56756a8d3d0b2
inder <sameerhamid213@gmail.com>
2024-07-17 10:46:04 +0530

    learn navigate through links

--- 19-learn-routing/src/App.js
+++ 19-learn-routing/src/App.js
@@ -8,21 +8,21 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 
 //old way
-const routeDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-);
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinitions);
 
 // new way
 // Create the router and provide it to the RouterProvider
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/products", element: <Products /> },
-// ]);
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Products /> },
+]);
 
-const router = createBrowserRouter(routeDefinitions);
 function App() {
   return <RouterProvider router={router} />;
 }
--- 19-learn-routing/src/pages/Home.jsx
+++ 19-learn-routing/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
-        <h1>My Home Page</h1>
+        <>
+            <h1>My Home Page</h1>
+            <p>Go to
+
+                <Link to={'/products'} > the list of products</Link>
+            </p>
+        </>
     )
 }
 
