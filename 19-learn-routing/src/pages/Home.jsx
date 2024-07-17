b15ebe8f8c7b2026726c04f67c7cb23d6cccc84b
inder <sameerhamid213@gmail.com>
2024-07-17 11:31:07 +0530

    programtically navigate to another route

--- 19-learn-routing/src/pages/Home.jsx
+++ 19-learn-routing/src/pages/Home.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 function Home() {
+    // another way of navigate
+    const navigate = useNavigate()
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         navigate('./products')
+    //     }, 3000)
+
+    // }, [])
+
+    const goToProducts = () => {
+        navigate('/products')
+    }
     return (
         <>
             <h1>My Home Page</h1>
             <p>Go to
                 <Link to={'/products'} > the list of products</Link>
+            </p>
+            <p>
+                <button onClick={goToProducts}>Products</button>
             </p>
         </>
     )
