02bcefc2ad0d4c0483116e631aeb009a6b2d9dcd
inder <sameerhamid213@gmail.com>
2024-07-17 11:50:38 +0530

    add product array

--- 19-learn-routing/src/pages/Products.jsx
+++ 19-learn-routing/src/pages/Products.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const PRODUCTS = [
+    { id: 'p1', name: 'Product 1' },
+    { id: 'p2', name: 'Product 2' },
+    { id: 'p3', name: 'Product 3' },
+    { id: 'p4', name: 'Product 4' },
+    { id: 'p5', name: 'Product 5' },
+    //... more products here  // this is just an example, replace with actual data from your API call or database query.
+]
 function Products() {
     return (
         <>
             <h1>My products page</h1>
             <ul>
                 <li>
-                    <Link to={'/products/1'}>Product 1</Link>
-                    <Link to={'/products/2'}>Product 2</Link>
-                    <Link to={'/products/3'}>Product 3</Link>
-                    <Link to={'/products/4'}>Product 4</Link>
-                    <Link to={'/products/5'}>Product 5</Link>
+                    {PRODUCTS.map(product => {
+                        return <Link to={`/products/${product.id}`}>Product 1</Link>
+                    })}
                 </li>
             </ul>
         </>
