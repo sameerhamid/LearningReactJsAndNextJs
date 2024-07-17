b5e296b69a0e0fe6573293fc0f79b62dc05f4103
inder <sameerhamid213@gmail.com>
2024-07-17 10:31:20 +0530

    add home route

--- 19-learn-routing/src/App.js
+++ 19-learn-routing/src/App.js
@@ -1,5 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+// Create the router and provide it to the RouterProvider
+const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
