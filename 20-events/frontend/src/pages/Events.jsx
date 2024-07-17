9ce2c21ae4062c5c66de2f8f426d14341063fb39
inder <sameerhamid213@gmail.com>
2024-07-17 17:23:43 +0530

    return respons from loader

--- 20-events/frontend/src/pages/Events.jsx
+++ 20-events/frontend/src/pages/Events.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-    const events = useLoaderData()
+    const { events } = useLoaderData()
 
     return (
         <>
@@ -19,7 +19,6 @@ export const loader = async () => {
     const response = await fetch("http://localhost:8080/events");
     if (!response.ok) {
     } else {
-        const resData = await response.json();
-        return resData.events;
+        return response
     }
 }
--- 20-events/frontend/src/pages/Root.jsx
+++ 20-events/frontend/src/pages/Root.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import MainNavigation from '../components/MainNavigation'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 
 function Root() {
+    // const navigation = useNavigation()
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/* {navigation.state === 'loading' && 'Loading...'} */}
                 <Outlet />
             </main>
         </>
