fc214d62581bd6d4cbf2f77182bc6e0868dacca1
inder <sameerhamid213@gmail.com>
2024-07-17 10:40:35 +0530

    learn the old way createRoutesFromElements

--- 19-learn-routing/src/App.js
+++ 19-learn-routing/src/App.js
@@ -1,12 +1,28 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 
+//old way
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
+
+// new way
 // Create the router and provide it to the RouterProvider
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/products", element: <Products /> },
-]);
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/products", element: <Products /> },
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 function App() {
   return <RouterProvider router={router} />;
 }
