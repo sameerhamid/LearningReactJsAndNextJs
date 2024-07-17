451aedbcacfe593b4146c84a6b08e8de44113380
inder <sameerhamid213@gmail.com>
2024-07-17 16:40:44 +0530

    outsource the loader logic

--- 20-events/frontend/src/App.js
+++ 20-events/frontend/src/App.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import NewEventPage from "./pages/NewEvent";
 import EventDetailsPage from "./pages/EventDetails";
 import EditEventPage from "./pages/EditEvent";
@@ -47,14 +47,7 @@ const router = createBrowserRouter([
             // path: "",
             index: true,
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-              if (!response.ok) {
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
 
           {
