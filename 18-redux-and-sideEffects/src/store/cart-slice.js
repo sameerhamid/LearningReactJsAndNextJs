8f441d9a12dfee25232cca9f931c202c5398e3e8
inder <sameerhamid213@gmail.com>
2024-07-22 11:15:51 +0530

    add removeItemFromCart action
    add cartReduce to store

--- 18-redux-and-sideEffects/src/store/cart-slice.js
+++ 18-redux-and-sideEffects/src/store/cart-slice.js
@@ -20,6 +20,19 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemToCart(state, action) {},
+    removeItemToCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
--- 18-redux-and-sideEffects/src/store/index.js
+++ 18-redux-and-sideEffects/src/store/index.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import uiReducer from "./ui-slice";
+import cartSlice from "./cart-slice";
+
 const store = configureStore({
   reducer: {
     ui: uiReducer,
+    cart: cartSlice.reducer,
   },
 });
 
