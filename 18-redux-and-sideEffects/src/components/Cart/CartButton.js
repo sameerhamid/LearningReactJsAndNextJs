6f2b85ad45655afbbb8838ffed13f3fcb973f190
inder <sameerhamid213@gmail.com>
2024-07-22 11:00:08 +0530

    add ui-slice
    used cart slice reducer and value in header and app comp

--- 18-redux-and-sideEffects/src/components/Cart/CartButton.js
+++ 18-redux-and-sideEffects/src/components/Cart/CartButton.js
@@ -1,8 +1,14 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const toogleCartHandler = () => {
+    dispatch(uiActions.toggleCart());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toogleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
