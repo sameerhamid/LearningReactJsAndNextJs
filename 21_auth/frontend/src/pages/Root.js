8bdd29701ba13b4e9bf9150b80a6460079401036
Sonoma <sonoma@iMac-Pro-2.local>
2024-07-29 13:38:45 +0530

    set expiration date on localStorage when login action is called
    create getTokenDuration method in utils and add check on if token is expired getAuthToken method
    
    add useEffect on rootLaout to check if the token has expired

--- 21_auth/frontend/src/pages/Root.js
+++ 21_auth/frontend/src/pages/Root.js
@@ -1,9 +1,36 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../utils/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuation = getTokenDuration();
+    console.log("tokenDuration>>>>", tokenDuation);
+    // clearing the token after 1 hour
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuation);
+  }, [token, submit]);
 
   return (
     <>
--- 21_auth/frontend/src/utils/auth.js
+++ 21_auth/frontend/src/utils/auth.js
@@ -1,7 +1,21 @@
 import { redirect } from "react-router-dom";
 
+export function getTokenDuration() {
+  const storedExpirationData = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationData);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+}
 export const getAuthTokent = () => {
   const token = localStorage.getItem("token");
+  const tokenDuration = getTokenDuration();
+  if (!token) {
+    return null;
+  }
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
   return token;
 };
 
