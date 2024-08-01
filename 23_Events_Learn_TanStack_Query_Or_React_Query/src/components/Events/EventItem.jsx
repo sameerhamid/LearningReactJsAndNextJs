c84ef1e7e0b0eb561c2259629def07b383311f27
Sonoma <sameerhamid213@gmail.com>
2024-08-01 16:46:01 +0530

    replace link viewDetails with button in eventItem comp

--- 23_Events_Learn_TanStack_Query_Or_React_Query/src/components/Events/EventItem.jsx
+++ 23_Events_Learn_TanStack_Query_Or_React_Query/src/components/Events/EventItem.jsx
@@ -1,11 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function EventItem({ event }) {
+  const navigate = useNavigate()
   const formattedDate = new Date(event.date).toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'short',
     year: 'numeric',
   });
+  const handleViewDetails = (e) => {
+    e.preventDefault()
+    navigate(`/events/${event.id}`)
+  }
   return (
     <article className="event-item">
       <img src={`http://localhost:3000/${event.image}`} alt={event.title} />
@@ -16,9 +21,13 @@ export default function EventItem({ event }) {
           <p className="event-item-location">{event.location}</p>
         </div>
         <p>
-          <Link to={`/events/${event.id}`} className="button">
+          {/* <Link to={`/events/${event.id}`} className="button">
             View Details
-          </Link>
+          </Link> */}
+
+          <form>
+            <button className="button" onClick={handleViewDetails}>View Details</button>
+          </form>
         </p>
       </div>
     </article>
