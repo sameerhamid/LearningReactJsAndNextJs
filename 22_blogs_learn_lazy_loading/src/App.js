d17d4da08f2ff2a0758eca15f5d1b9851a4cbd59
Sonoma <sonoma@iMac-Pro-2.local>
2024-08-01 15:42:17 +0530

    add loazy loading for blogs and posts page

--- 22_blogs_learn_lazy_loading/src/App.js
+++ 22_blogs_learn_lazy_loading/src/App.js
@@ -1,13 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
+// import BlogPage, { loader as postsLoader } from "./pages/Blog";
+import HomePage from "./pages/Home";
+// import PostPage, { loader as postLoader } from "./pages/Post";
+import RootLayout from "./pages/Root";
+import { lazy, Suspense } from "react";
+
+// lazy loading: load comp when we go on that route
+const BlogPage = lazy(() => import("./pages/Blog"));
+const PostPage = lazy(() => import("./pages/Post"));
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
@@ -15,10 +20,33 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'posts',
+        path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense
+                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
+              >
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            element: (
+              <Suspense
+                fallback={<p style={{ textAlign: "center" }}>Loading...</p>}
+              >
+                <PostPage />
+              </Suspense>
+            ),
+            // lazy loading: load module when we go on that route
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
