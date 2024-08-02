feb0358671bdde3a2dc214d4b8ae204d57533598
Sonoma <sameerhamid213@gmail.com>
2024-08-02 12:00:05 +0530

    use queryMutation in newEvent comp
    add loadin and error blocks

--- 23_Events_Learn_TanStack_Query_Or_React_Query/src/utils/http.js
+++ 23_Events_Learn_TanStack_Query_Or_React_Query/src/utils/http.js
@@ -17,3 +17,23 @@ export async function fetchEvents({ signal, searchTerm }) {
 
   return events;
 }
+
+export async function createNewEvent(eventData) {
+  const response = await fetch("http://localhost:3000/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occured while creating the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+  return event;
+}
