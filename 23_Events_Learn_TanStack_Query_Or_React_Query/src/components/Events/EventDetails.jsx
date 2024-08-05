a4772b79508316d73a3b5ca4fa401a66cf670150
Sonoma <sameerhamid213@gmail.com>
2024-08-05 13:18:06 +0530

    add delete conformation modal and handle loading and error state when use confirm delete button

--- 23_Events_Learn_TanStack_Query_Or_React_Query/src/components/Events/EventDetails.jsx
+++ 23_Events_Learn_TanStack_Query_Or_React_Query/src/components/Events/EventDetails.jsx
@@ -1,84 +1,142 @@
-import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { Link, Outlet, useNavigate, useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { deleteEvent, fetchEvent, queryClient } from '../../utils/http.js';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
+import Header from "../Header.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent, queryClient } from "../../utils/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { useState } from "react";
+import Modal from "../UI/Modal.jsx";
 
 export default function EventDetails() {
-  const params = useParams()
-  const navigate = useNavigate()
+  const [isDeleting, setIsDeleting] = useState(false);
+  const params = useParams();
+  const navigate = useNavigate();
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['events', params.id],
-    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal })
-  })
+    queryKey: ["events", params.id],
+    queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
+  });
 
-  const { mutate } = useMutation({
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeletion,
+    error: errorDeletion,
+  } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: ['events'],
-        refetchType: 'none'
-      })
-      navigate('/events')
-    }
-  })
+        queryKey: ["events"],
+        refetchType: "none",
+      });
+      navigate("/events");
+    },
+  });
 
   const handleDeleteEvent = () => {
-    mutate({ id: params.id })
-  }
+    mutate({ id: params.id });
+  };
+
+  const handleStartDeleting = () => {
+    setIsDeleting(true);
+  };
+  const handleStopDeleting = () => {
+    setIsDeleting(false);
+  };
 
   let content;
   if (isPending) {
-    content = <div id='event-details-content' className='center'>
-      <p>Fetching event data...</p>
-    </div>
+    content = (
+      <div id="event-details-content" className="center">
+        <p>Fetching event data...</p>
+      </div>
+    );
   }
 
   if (isError) {
-    content = <div id='event-details-content' className='center'>
-      <ErrorBlock title={"Failed to load event"} description={error.info?.message || "Failed to fetch event data, please try again later."} />
-    </div>
+    content = (
+      <div id="event-details-content" className="center">
+        <ErrorBlock
+          title={"Failed to load event"}
+          description={
+            error.info?.message ||
+            "Failed to fetch event data, please try again later."
+          }
+        />
+      </div>
+    );
   }
 
   if (data) {
-    const formattedDate = new Date(data.date).toLocaleDateString('en-US', {
-      day: 'numeric',
-      month: 'short',
-      year: 'numeric',
-    })
-    content = <>
-      <header>
-        <h1>{data.title}</h1>
-        <nav>
-          <button onClick={handleDeleteEvent}>Delete</button>
-          <Link to="edit">Edit</Link>
-        </nav>
-      </header>
-      <div id="event-details-content">
-        <img src={`http://localhost:3000/${data.image}`} alt="" />
-        <div id="event-details-info">
-          <div>
-            <p id="event-details-location">{data.location}</p>
-            <time dateTime={`Todo-DateT$Todo-Time`}>{formattedDate} @ {data.time}</time>
+    const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleStartDeleting}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt="" />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>
+                {formattedDate} @ {data.time}
+              </time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
           </div>
-          <p id="event-details-description">{data.description}</p>
         </div>
-      </div>
-    </>
-
+      </>
+    );
   }
   return (
     <>
+      {isDeleting && (
+        <Modal onClose={handleStopDeleting}>
+          <h2>Are you sure</h2>
+          <p>
+            Do you really want to delete this event? This action cannot be
+            undone.
+          </p>
+          <div className="form-actions">
+            {isPendingDeletion && <p>Deleting event...</p>}
+            {!isPendingDeletion && (
+              <>
+                <button onClick={handleStopDeleting} className="button-text">
+                  Cancel
+                </button>
+                <button onClick={handleDeleteEvent} className="button">
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+
+          {isErrorDeletion && (
+            <ErrorBlock
+              title={"Failed to delete event"}
+              description={
+                errorDeletion.info?.message ||
+                "Failed to delete event, please try again later."
+              }
+            />
+          )}
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        {content}
-      </article>
+      <article id="event-details">{content}</article>
     </>
   );
 }
