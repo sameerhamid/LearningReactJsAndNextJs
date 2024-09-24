64fd43c4e1c53ff7ee01c90755f59e3f15cc435a
Sonoma <sameerhamid213@gmail.com>
2024-09-24 16:50:48 +0530

    add not-found page

---
+++ 25-foodies-next-js/app/not-found.js
@@ -0,0 +1,10 @@
+function PageNotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>Unfortunately, we could not find the requested page or resourse.</p>
+    </main>
+  );
+}
+
+export default PageNotFound;
