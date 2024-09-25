b096c49d29ec0bcdce1045ec3f329d9a0f35e355
Sonoma <sameerhamid213@gmail.com>
2024-09-25 12:36:28 +0530

    add call fucntion notFound

--- 25-foodies-next-js/app/meals/[mealSlug]/page.js
+++ 25-foodies-next-js/app/meals/[mealSlug]/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 function MealsPageById({ params }) {
   const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
   return (
     <>
