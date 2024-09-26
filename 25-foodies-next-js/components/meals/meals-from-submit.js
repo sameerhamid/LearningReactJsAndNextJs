e0f1ca9a37ab5149f8118a0a25e9a11f443df93f
Sonoma <sameerhamid213@gmail.com>
2024-09-26 18:06:43 +0530

    add MealsFormSubmit componet to show use useFormStatus hook from react-dom to show submiting

---
+++ 25-foodies-next-js/components/meals/meals-from-submit.js
@@ -0,0 +1,12 @@
+"use client";
+import { useFormStatus } from "react-dom";
+function MealsFormSubmit() {
+  const { pending } = useFormStatus();
+  return (
+    <button type="submit" disabled={pending}>
+      {pending ? "Submitting..." : "Share Meal"}
+    </button>
+  );
+}
+
+export default MealsFormSubmit;
