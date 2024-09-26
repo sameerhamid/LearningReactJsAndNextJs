a9b840c8d8ce3b2c3184c860e102e50fa6b8cda9
Sonoma <sameerhamid213@gmail.com>
2024-09-26 16:40:16 +0530

    add shareMeal as server function
    add changes in imagePikcer component

--- 25-foodies-next-js/components/meals/image-picker.js
+++ 25-foodies-next-js/components/meals/image-picker.js
@@ -1,20 +1,47 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 export default function ImagePicker({ label, name }) {
   const inputImageRef = useRef();
+  const [pickedImage, setPickedImage] = useState();
   function handleImagePick() {
     inputImageRef.current.click();
   }
+  function handleInputImageChange(e) {
+    const file = e.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setPickedImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
+
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              fill
+              alt="The image seleceted by the user"
+              src={pickedImage}
+            />
+          )}
+        </div>
         <input
+          onChange={handleInputImageChange}
           className={classes.input}
           type="file"
           id={name}
-          accept="/image/png, /image/jpeg"
+          // accept="/image/png, /image/jpeg"
           name={name}
           ref={inputImageRef}
         />
