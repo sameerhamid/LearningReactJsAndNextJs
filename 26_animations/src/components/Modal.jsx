e24cb815d0102b86e33f63d50627bcb078f44676
Sonoma <sameerhamid213@gmail.com>
2024-10-01 16:33:14 +0530

    add wrap the NewChallenge into AnimatePrsence compnent from framer motion to show the animation when exit

--- 26_animations/src/components/Modal.jsx
+++ 26_animations/src/components/Modal.jsx
@@ -6,7 +6,9 @@ export default function Modal({ title, children, onClose }) {
       <div className="backdrop" onClick={onClose} />
       <motion.dialog open className="modal"
         initial={{ opacity: 0, y: 30 }} // it allows us to set the starting state of this element
-        animate={{ opacity: 1, y: 0 }}>
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 30 }} // when the elemen is removed from the dom
+      >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
