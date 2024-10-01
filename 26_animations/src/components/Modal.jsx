7f777030408641eeb566028bd4212f9b204b3d3c
Sonoma <sameerhamid213@gmail.com>
2024-10-01 16:26:33 +0530

    add animton for modal when apears to dom

--- 26_animations/src/components/Modal.jsx
+++ 26_animations/src/components/Modal.jsx
@@ -1,13 +1,15 @@
 import { createPortal } from 'react-dom';
-
+import { motion } from 'framer-motion'
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog open className="modal"
+        initial={{ opacity: 0, y: 30 }} // it allows us to set the starting state of this element
+        animate={{ opacity: 1, y: 0 }}>
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
