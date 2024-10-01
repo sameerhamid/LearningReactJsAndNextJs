4b73f6ecbf41bb7b172aa82c8a42628a93d4a951
Sonoma <sameerhamid213@gmail.com>
2024-10-01 16:46:01 +0530

    add variants prop to add variable in modal

--- 26_animations/src/components/Modal.jsx
+++ 26_animations/src/components/Modal.jsx
@@ -4,14 +4,26 @@ export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <motion.dialog open className="modal"
+      {/* <motion.dialog open className="modal"
         initial={{ opacity: 0, y: 30 }} // it allows us to set the starting state of this element
         animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: 30 }} // when the elemen is removed from the dom
+        exit={{opacity: 0, y: 30}} // when the elemen is removed from the dom
+      > */}
+
+      <motion.dialog open className="modal"
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 }
+        }}
+
+        initial="hidden"// it allows us to set the starting state of this element
+        animate="visible"
+        exit="hidden" // when the elemen is removed from the dom
       >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
+      {/* </motion.dialog> */}
     </>,
     document.getElementById('modal')
   );
