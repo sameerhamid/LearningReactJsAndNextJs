d890626e83882a2b67b6b5e47f367b02150c215a
Sonoma <sameerhamid213@gmail.com>
2024-10-01 16:38:24 +0530

    add whileHover prop to add scale animation to add challenge button

--- 26_animations/src/components/Header.jsx
+++ 26_animations/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import NewChallenge from './NewChallenge.jsx';
 
 export default function Header() {
@@ -21,9 +21,15 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button onClick={handleStartAddNewChallenge} className="button"
+          whileHover={{ scale: 1.1 }}
+          transition={{
+            type: 'spring', stiffness: 500,
+            // mass: 100
+          }}
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
