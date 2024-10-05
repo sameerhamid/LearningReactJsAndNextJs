81e72ad88169a1d321847da130e7517423ed4280
Sonoma <sameerhamid213@gmail.com>
2024-10-05 11:54:38 +0530

    fix when switch tab from zero item to some items animation

--- 26_animations/src/components/Challenges.jsx
+++ 26_animations/src/components/Challenges.jsx
@@ -44,6 +44,8 @@ export default function Challenges() {
           {displayedChallenges.length > 0 && (
             <motion.ol className="challenge-items"
               key="list"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
               exit={{ y: -30, opacity: 0 }}>
               <AnimatePresence >
                 {displayedChallenges.map((challenge) => (
