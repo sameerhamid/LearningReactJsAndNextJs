d6b3d13043a06a40d714d977b2612364f37a43eb
Sonoma <sameerhamid213@gmail.com>
2024-10-07 17:59:48 +0530

    add commenting about compount components

--- 28-react-patterns-&-practices/src/App.jsx
+++ 28-react-patterns-&-practices/src/App.jsx
@@ -1,3 +1,10 @@
+
+/**
+ *  learn about compound componets
+ Multilple components that don't work standalone but instead together 
+ eg: html select and option element
+ * 
+ */
 function App() {
   return <h1>React Patterns & Practices</h1>;
 }
