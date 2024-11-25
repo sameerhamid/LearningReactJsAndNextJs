b7d9c66aed79685026e58d9edf89a7506437e6f1
Sonoma <sameerhamid213@gmail.com>
2024-11-25 15:12:25 +0530

    add initStore and dispatch fuction

--- 29_replace-redux-with-contextApi_contextApi-to-hooks-store/hook-store/store.js
+++ 29_replace-redux-with-contextApi_contextApi-to-hooks-store/hook-store/store.js
@@ -4,8 +4,29 @@ let globalState = {};
 let listners = [];
 let actions = {};
 
-const useStore = () => {
+/**
+ * Hook to connect to the global state.
+ * @return {Array} The first element of the array is the global state,
+ *                 the second element is the dispatch function.
+ */
+export const useStore = () => {
   const setState = useState(globalState)[1];
+
+  /**
+   * Dispatches an action.
+   * @param {string} actionIdentifier - The identifier of the action to dispatch.
+   * @return {void}
+   */
+  const dispatch = (actionIdentifier) => {
+    const newState = actions[actionIdentifier](globalState);
+    globalState = {
+      ...globalState,
+      newState,
+    };
+    for (let listener of listners) {
+      listener(globalState);
+    }
+  };
 
   useEffect(() => {
     listners.push(setState);
@@ -14,4 +35,21 @@ const useStore = () => {
       listners = listners.filter((l) => l !== setState);
     };
   }, [setState]);
+
+  return [globalState, dispatch];
+};
+
+/**
+ * Initializes the store with user provided actions and an initial state.
+ * @param {Object} userActions An object with action identifiers as keys and
+ * functions as values. The functions will be called with the current global state
+ * and should return a new state.
+ * @param {Object} [initialState] An object with the initial state of the store.
+ * This is optional and if not provided the store will start with an empty state.
+ */
+export const initSore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
 };
