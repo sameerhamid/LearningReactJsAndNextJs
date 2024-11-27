772b143acf87b981ac8c3871b2d1b56e921d38fc
Sonoma <sameerhamid213@gmail.com>
2024-11-27 13:08:07 +0530

    add test suit in greeting test

--- 31_learn_testing/src/components/Greeting.test.js
+++ 31_learn_testing/src/components/Greeting.test.js
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders hello world as a text", () => {
-  // Arrange
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders hello world as a text", () => {
+    // Arrange
+    render(<Greeting />);
 
-  // Act
-  // ...nothing
+    // Act
+    // ...nothing
 
-  // Assert
-  const helloWorld = screen.getByText("Hello world!", { exact: false });
-  expect(helloWorld).toBeInTheDocument();
+    // Assert
+    const helloWorld = screen.getByText("Hello world!", { exact: false });
+    expect(helloWorld).toBeInTheDocument();
+  });
 });
