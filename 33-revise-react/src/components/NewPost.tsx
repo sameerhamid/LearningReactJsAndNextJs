d5df0099b6c2c976aed7d3da136c1a5f1668eef9
Sonoma <sameerhamid213@gmail.com>
2024-12-02 12:12:48 +0530

    revise state

--- 33-revise-react/src/components/NewPost.tsx
+++ 33-revise-react/src/components/NewPost.tsx
@@ -2,18 +2,36 @@ import { useState } from "react";
 import classes from "./NewPost.module.css";
 
 function NewPost() {
+  const [name, setName] = useState("");
+  const [body, setBody] = useState("");
+
   const changeBodyHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    console.log(event.target.value);
+    setBody(event.target.value);
+  };
+  const changeNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
   };
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea
+          id="body"
+          required
+          rows={3}
+          onChange={changeBodyHandler}
+          value={body}
+        />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input
+          type="text"
+          id="name"
+          required
+          onChange={changeNameHandler}
+          value={name}
+        />
       </p>
     </form>
   );
