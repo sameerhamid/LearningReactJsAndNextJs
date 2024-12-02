cfb3cc82932a41ad58522a5ef9a34214f03ea83c
Sonoma <sameerhamid213@gmail.com>
2024-12-02 12:36:45 +0530

    revise lifting state up

--- 33-revise-react/src/components/NewPost.tsx
+++ 33-revise-react/src/components/NewPost.tsx
@@ -1,40 +1,22 @@
-import { useState } from "react";
 import classes from "./NewPost.module.css";
-
-function NewPost() {
-  const [name, setName] = useState("");
-  const [body, setBody] = useState("");
-
-  const changeBodyHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setBody(event.target.value);
-  };
-  const changeNameHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value);
-  };
+interface NewPostPropsType {
+  onBodyChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onAuthorChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const NewPost: React.FC<NewPostPropsType> = (props) => {
+  const { onBodyChange, onAuthorChange } = props;
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea
-          id="body"
-          required
-          rows={3}
-          onChange={changeBodyHandler}
-          value={body}
-        />
+        <textarea id="body" required rows={3} onChange={onBodyChange} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input
-          type="text"
-          id="name"
-          required
-          onChange={changeNameHandler}
-          value={name}
-        />
+        <input type="text" id="name" required onChange={onAuthorChange} />
       </p>
     </form>
   );
-}
+};
 
 export default NewPost;
--- 33-revise-react/src/components/PostList.tsx
+++ 33-revise-react/src/components/PostList.tsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 const PostList = () => {
+  const [enteredAuthor, setEnteredAuthor] = useState<string>("");
+  const [enteredBody, setEnteredBody] = useState<string>("");
+
+  const changeBodyHandler = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEnteredBody(event.target.value);
+  };
+  const changeAuthorHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEnteredAuthor(event.target.value);
+  };
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler}
+      />
       <ul className={classes.posts}>
-        <Post body="My First Post" author="Max" />
-        <Post body="My Second Post" author="Max" />
+        <Post body={enteredBody} author={enteredAuthor} />
       </ul>
     </>
   );
