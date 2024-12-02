95ff2b3e61a0a6d365a052f0cb25eaea32b002e6
Sonoma <sameerhamid213@gmail.com>
2024-12-02 11:02:12 +0530

    revise render dynimac values

--- 33-revise-react/src/components/Post.tsx
+++ 33-revise-react/src/components/Post.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+const names = ["First name", "Last name"];
 const Post: React.FC = (props) => {
+  const choosenName = Math.random() > 0.5 ? names[0] : names[1];
   const {} = props;
   return (
     <div>
       <p>Hello</p>
-      <p>Reactjs is awesome!</p>
+      <p>{choosenName}</p>
     </div>
   );
 };
