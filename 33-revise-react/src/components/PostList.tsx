420050b271ca2c2433e865296efbf387c6869677
Sonoma <sameerhamid213@gmail.com>
2024-12-02 11:58:17 +0530

    add newPost component

--- 33-revise-react/src/components/PostList.tsx
+++ 33-revise-react/src/components/PostList.tsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 const PostList = () => {
   return (
-    <ul className={classes.posts}>
-      <Post body="My First Post" author="Max" />
-      <Post body="My Second Post" author="Max" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post body="My First Post" author="Max" />
+        <Post body="My Second Post" author="Max" />
+      </ul>
+    </>
   );
 };
 
