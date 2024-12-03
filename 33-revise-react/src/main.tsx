83ed35a4564da814b0815f6eef324368ed60dfb6
Sonoma <sameerhamid213@gmail.com>
2024-12-03 11:22:04 +0530

    revise Adding Routes

--- 33-revise-react/src/main.tsx
+++ 33-revise-react/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Routes from "./utils/routes.ts";
+import NewPost from "./components/NewPost.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: Routes.Home,
+    element: <App />,
+  },
+  {
+    path: Routes.NewPost,
+    element: <NewPost onCancel={() => {}} onAddPost={() => {}} />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ 33-revise-react/src/utils/routes.ts
@@ -0,0 +1,7 @@
+const Routes = {
+  Home: "/",
+  NewPost: "/new-post",
+  NotFound: "/404",
+} as const;
+
+export default Routes;
