604c94948b7069195519e5f930a0bf3bcb0b6220
Sonoma <sameerhamid213@gmail.com>
2024-12-06 12:10:45 +0530

    add expenseitem component

---
+++ 34-react-updates-and-best-practices/src/components/ExpenseItem.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import classes from "./ExpenseItem.module.css";
+export interface ExpenseItemType {
+  id: string;
+  title: string;
+  amount: number;
+  date: Date;
+}
+
+interface ExpenseItemPropsType {
+  item: ExpenseItemType;
+}
+const ExpenseItem: React.FC<ExpenseItemPropsType> = (props) => {
+  const { title, amount, date } = props.item;
+  return (
+    <div className={classes["expense-item"]}>
+      <div>{date.toISOString()}</div>
+      <div className={classes["expense-item__description"]}>
+        <h2>{title}</h2>
+        <div className={classes["expense-item__price"]}>${amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
