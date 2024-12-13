013b63af4ff60544a002f0493af24727a6aef5f7
Sonoma <sameerhamid213@gmail.com>
2024-12-13 11:31:08 +0530

    add UserInput component

--- 35-investment-calculator/src/App.tsx
+++ 35-investment-calculator/src/App.tsx
@@ -1,6 +1,6 @@
 import Header from "@root/components/Header";
 import ResultsTable from "@root/components/ResultsTable";
-
+import UserInput from "@root/components/UserInput";
 function App() {
   const formatter = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -42,41 +42,7 @@ function App() {
     <div>
       <Header />
 
-      <form className="form">
-        <div className="input-group">
-          <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
-          </p>
-          <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
-          </p>
-        </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
-            Reset
-          </button>
-          <button type="submit" className="button">
-            Calculate
-          </button>
-        </p>
-      </form>
-
-      {/* Todo: Show below table conditionally (only once result data is available) */}
-      {/* Show fallback text if no data is available */}
+      <UserInput />
 
       <ResultsTable />
     </div>
---
+++ 35-investment-calculator/src/components/UserInput/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import classes from "./styles.module.css";
+
+const UserInput: React.FC = () => {
+  return (
+    <div>
+      <form className={classes.form}>
+        <div className={classes["input-group"]}>
+          <p>
+            <label htmlFor="current-savings">Current Savings ($)</label>
+            <input type="number" id="current-savings" />
+          </p>
+          <p>
+            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+            <input type="number" id="yearly-contribution" />
+          </p>
+        </div>
+        <div className={classes["input-group"]}>
+          <p>
+            <label htmlFor="expected-return">
+              Expected Interest (%, per year)
+            </label>
+            <input type="number" id="expected-return" />
+          </p>
+          <p>
+            <label htmlFor="duration">Investment Duration (years)</label>
+            <input type="number" id="duration" />
+          </p>
+        </div>
+        <p className={classes.actions}>
+          <button type="reset" className={classes.buttonAlt}>
+            Reset
+          </button>
+          <button type="submit" className={classes.button}>
+            Calculate
+          </button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default UserInput;
