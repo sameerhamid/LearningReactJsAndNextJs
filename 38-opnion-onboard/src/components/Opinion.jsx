0b780027feeb7517db6a94332ff720ad388a58db
Sonoma <sameerhamid213@gmail.com>
2024-12-17 12:10:09 +0530

    Registering Multiple Form Actions in ipinion component

--- 38-opnion-onboard/src/components/Opinion.jsx
+++ 38-opnion-onboard/src/components/Opinion.jsx
@@ -1,4 +1,13 @@
 export function Opinion({ opinion: { id, title, body, userName, votes } }) {
+
+  const upvoteAction = (formData) => {
+    event.preventDefault();
+  }
+
+  const downvoteAction = (formData) => {
+    event.preventDefault();
+  }
+
   return (
     <article>
       <header>
@@ -7,7 +16,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
       </header>
       <p>{body}</p>
       <form className="votes">
-        <button>
+        <button formAction={upvoteAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -27,7 +36,7 @@ export function Opinion({ opinion: { id, title, body, userName, votes } }) {
 
         <span>{votes}</span>
 
-        <button>
+        <button formAction={downvoteAction}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
