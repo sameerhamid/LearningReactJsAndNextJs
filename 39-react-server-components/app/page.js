b62be7723a4918913942115be8f972feea6fe97b
Sonoma <sameerhamid213@gmail.com>
2024-12-18 11:31:39 +0530

    add UserPromises compeonent

--- 39-react-server-components/app/page.js
+++ 39-react-server-components/app/page.js
@@ -2,15 +2,21 @@ import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from "@/components/RSCDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
+import UserPromises from "@/components/UserPromises";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main>
       {/* <ClientDemo>
         <RSCDemo />
       </ClientDemo> */}
-      <DataFetchingDemo />
-      <ServerActionsDemo />
+      {/* <DataFetchingDemo />
+      <ServerActionsDemo /> */}
+
+      <Suspense fallback={<p>Loading users...</p>}>
+        <UserPromises />
+      </Suspense>
     </main>
   );
 }
---
+++ 39-react-server-components/components/UserPromises.js
@@ -0,0 +1,22 @@
+import fs from "node:fs/promises";
+const UserPromises = async () => {
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+  const data = await fs.readFile("dummy-db.json", "utf-8");
+  const users = JSON.parse(data);
+  return (
+    <div className="rsc">
+      <h2>RSC with data fetching</h2>
+      <p>
+        User <strong>async / await</strong>
+        for data fetching.
+      </p>
+      {users.map((user) => (
+        <li>
+          {user.name} ({user.title})
+        </li>
+      ))}
+    </div>
+  );
+};
+
+export default UserPromises;
