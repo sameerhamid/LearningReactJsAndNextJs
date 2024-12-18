0148155fdfbccd4c013bbef1d58ce35e9c9e9d3f
Sonoma <sameerhamid213@gmail.com>
2024-12-18 11:43:21 +0530

    The use() Hook In Action

--- 39-react-server-components/app/page.js
+++ 39-react-server-components/app/page.js
@@ -4,8 +4,15 @@ import RSCDemo from "@/components/RSCDemo";
 import ServerActionsDemo from "@/components/ServerActionsDemo";
 import UserPromises from "@/components/UserPromises";
 import { Suspense } from "react";
-
+import fs from "node:fs/promises";
 export default async function Home() {
+  const fetchingUsersPromise = await new Promise((resolve, reject) =>
+    setTimeout(async () => {
+      const data = await fs.readFile("dummy-db.json", "utf-8");
+      resolve(JSON.parse(data));
+    }, 1500)
+  );
+
   return (
     <main>
       {/* <ClientDemo>
@@ -15,7 +22,7 @@ export default async function Home() {
       <ServerActionsDemo /> */}
 
       <Suspense fallback={<p>Loading users...</p>}>
-        <UserPromises />
+        <UserPromises usersPromise={fetchingUsersPromise} />
       </Suspense>
     </main>
   );
